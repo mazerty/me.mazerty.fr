37e0c3513b1ad154c3f375887f77efd32c971612
mazerty <mazerty@gmail.com>
2021-11-06 17:25:16 +0100

    added ubuntu font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,12 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 module.exports = {
-    purge: ["./index.html", "./src/**/*.jsx"]
+    purge: ["./index.html", "./src/**/*.jsx"],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Ubuntu", ...defaultTheme.fontFamily.sans]
+            }
+        }
+    }
 }
